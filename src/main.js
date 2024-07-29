98f574dde6441dc3aca7b20166c9bdcc893649e6
Volodymyr Naumchyk <162045667+NaumchykV@users.noreply.github.com>
2024-07-29 10:08:07 +0300

    hw11 upd1

--- src/main.js
+++ src/main.js
@@ -15,26 +15,32 @@ function handleSubmit(event) {
   loader.classList.remove('hiden');
   event.preventDefault();
   let searchWord = input.value.trim();
-  searchImages(`${searchWord}`).then(data => {
-    if (data.total === 0) {
+  searchImages(`${searchWord}`)
+    .then(data => {
+      if (data.total === 0) {
+        iziToast.error({
+          position: 'topRight',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+      if (searchWord === '') {
+        iziToast.error({
+          position: 'topRight',
+          message: 'Please fill the input',
+        });
+      } else {
+        createImages(data);
+      }
+    })
+    .catch(error => {
       iziToast.error({
         position: 'topRight',
         message:
-          'Sorry, there are no images matching your search query. Please try again!',
+          'Виникла помилка під час отримання зображень. Будь ласка, спробуйте пізніше.',
       });
+    })
+    .finally(() => {
       loader.classList.add('hiden');
-      return;
-    }
-    if (searchWord === '') {
-      iziToast.error({
-        position: 'topRight',
-        message: 'Please fill the input',
-      });
-      loader.classList.add('hiden');
-      return;
-    } else {
-      createImages(data);
-    }
-    loader.classList.add('hiden');
-  });
+    });
 }
